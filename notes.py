03c8ec82ba131641b1ba34acf079d1610dfb5bce
Robin Kuehn <kuehn.robin@gmail.com>
2017-07-09 21:50:05 -0700

    create initial python notes

---
+++ notes.py
@@ -0,0 +1,17 @@
+
+#type python in terminal to get into python shell >>> to get out type exit()
+# variables
+name = "Robin";
+num = 1;
+
+#functions
+print("Hello, World");
+
+#indendtation should be 4 spaces
+#blocks start with a colon
+#variable and functions should all be lower case with no hyphens, just underscores
+
+def answer_the_phone():
+    greeting = "Hello there";
+    print(greeting);
+
