32dbaf82fbfd846bd1bd897218508d82b9aadc4f
Robin Kuehn <kuehn.robin@gmail.com>
2017-07-22 16:24:28 -0400

    #4 divisors complete

--- exercises.py
+++ exercises.py
@@ -37,6 +37,19 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 def lessThan5(list):
   print([num for num in list if num < 5])
 
+#4 Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+def divisors():
+  try:
+    num = int(input("pick a number"));
+  except ValueError:
+    print("please enter a number")
+  for i in range(1, num + 1):
+    if num % i == 0:
+      print(i)
+
+divisors()
+
 
 
 
