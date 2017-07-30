1fda9fba7adf314e2612eb7bfc60a6a3a0e3eb7c
Robin Kuehn <kuehn.robin@gmail.com>
2017-07-29 21:13:56 -0700

    #15 sentence reverse complete

--- exercises.py
+++ exercises.py
@@ -166,10 +166,10 @@ def return_set(listItem):
 
 
 
+#15 Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order. 
 
-
-
-
+def reverse_sentence(sentence):
+  print(sentence.split()[::-1])
 
 
 
